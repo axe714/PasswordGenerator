f308d2fcc16d0ff9b1a4b0760104a66215d1cf4b
Axe <axearzadon@gmail.com>
2022-04-04 21:16:31 -0700

    created 4 arrays (lower case letters, capitalized letters, numbers, special characters), added confirm/alert methods

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-
-  var generatePassword = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '0',
+var generatePassword = [
     'q',
     'w',
     'e',
@@ -43,7 +28,22 @@ function writePassword() {
     'b',
     'n',
     'm',
-    '`',
+];
+
+var generatePasswordNumbers = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+]
+
+var generatePasswordSpecial = [
     '!',
     '@',
     '#',
@@ -58,17 +58,86 @@ function writePassword() {
     '_',
     '=',
     '+',
-    '[',
     '{',
-    ']',
     '}',
+    '[',
+    ']',
+    '|',
+    ':',
     ';',
-    '',
-  ]
-  
-console.log(generatePassword[1])
+    '"',
+    '<',
+    '>',
+    ',',
+    '.',
+    '/',
+    '?',
+    '~',
+    '`',
+]
 
+var generatePasswordCapital = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+];
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = prompt("How long would you like your password to be? (8-128 characters)");
+
+    if (password >= 8 && password <= 128) {
+    confirm("Would you like to include numbers?");
+  }  
+
+  if (password >= 8 && password <= 128) {
+    confirm("Would you like to include capitalized letters?")
+  }
+
+  if (password >= 8 && password <= 128) {
+    confirm("Would you like to include special characters?");
+  } 
+
+  else (password.length < 8 || password.length > 128); {
+    alert("Password must be between 8 and 128 characters");
+  }
+  return;
+
+  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
+
+
+
+
+    
+    
 
   passwordText.value = password;
 
@@ -76,3 +145,7 @@ console.log(generatePassword[1])
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function myFunction() {
+  confirm("Hello! I am an alert box!");
+}
